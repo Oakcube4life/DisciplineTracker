b0a1728872e49eb184689dbd72095e0ea1cd1c4b
Gavin MacFadyen <gjmacfadyen@outlook.com>
2025-08-29 16:23:14 -0300

    Initial Commit

---
+++ DisciplineTracker/DisciplineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisciplineTrackerApp.swift
+//  DisciplineTracker
+//
+//  Created by Gavin MacFadyen on 2025-08-29.
+//
+
+import SwiftUI
+
+@main
+struct DisciplineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisciplineTrackerTests/DisciplineTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisciplineTrackerTests.swift
+//  DisciplineTrackerTests
+//
+//  Created by Gavin MacFadyen on 2025-08-29.
+//
+
+import Testing
+@testable import DisciplineTracker
+
+struct DisciplineTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
